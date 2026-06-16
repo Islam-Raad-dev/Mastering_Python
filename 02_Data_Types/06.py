1d04200ba97e7c9343cb845e2697e34279b2b620
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-06-16 18:58:47 +0300

    Strings Methods  _ 2

--- 02_Data_Types/06.py
+++ 02_Data_Types/06.py
@@ -1,7 +1,7 @@
 # ---------------------
 # -- Strings Methods --
 # ---------------------
-
+                                                       
 # split() rsplit()
 
 a = "I Love Python and C++ and PostrgreSQL"
