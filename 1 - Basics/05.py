6603b442dfef2df37b24a9e15749c5597c944f48
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-07 11:51:37 +0300

    Strings Methods

--- 1 - Basics/05.py
+++ 1 - Basics/05.py
@@ -54,3 +54,126 @@ print(g.upper())
 h = "Islam"
 
 print(h.lower())
+
+# split() rsplit()
+
+a = "I Love Python and C++ and PostrgreSQL"
+print(a.split())
+
+b = "I-Love-Python-and-C++-and-PostrgreSQL"
+print(b.split("-"))
+
+c = "I-Love-Python-and-C++-and-PostrgreSQL"
+print(c.split("-", 3))
+
+d = "I-Love-Python-and-C++-and-PostrgreSQL"
+print(d.rsplit("-", 3))
+
+# center()
+
+e = "Islam"
+print(e.center(9))  # Spaces
+print(e.center(9, "#"))  # Hashes
+print(e.center(15, "@"))  # @
+
+# count()
+
+f = "I Love Python and C++ Because C++ is Easy"
+print(f.count("C++"))  # 2 C++ Words
+print(f.count("C++", 0, 25))  # Only One C++ Word
+
+# swapcase()
+
+g = "I Love Python"
+h = "i lOVE pYTHON"
+
+print(g.swapcase())
+print(h.swapcase())
+
+# startswith()
+
+i = "I Love Python"
+print(i.startswith("I"))
+print(i.startswith("S"))
+print(i.startswith("P", 7, 12))
+
+# endswith()
+
+j = "I Love Python"
+print(j.endswith("n"))
+print(j.endswith("S"))
+print(j.endswith("e", 2, 6))
+
+# index(SubString, Start, End)
+
+a = "I Love Python"
+# print(a.index("P"))  # Index Number 7
+# print(a.index("P", 0, 10))  # Index Number 7
+# print(a.index("P", 0, 5))  # Through Error
+
+# find(SubString, Start, End)
+
+b = "I Love Python"
+print(b.find("P"))  # Index Number 7
+print(b.find("P", 0, 10))  # Index Number 7
+print(b.find("P", 0, 5))  # -1
+
+# rjust(Width, Fill Char) ljust(Width, Fill Char)
+
+c = "Islam"
+print(c.rjust(10))
+print(c.rjust(10, "#"))
+
+d = "Islam"
+print(d.ljust(10))
+print(d.ljust(10, "#"))
+
+# splitlines()
+
+e = """First Line
+Second Line
+Third Line"""
+
+print(e.splitlines())
+
+f = "First Line\nSecond Line\nThird Line"
+
+print(f.splitlines())
+
+# expandtabs()
+
+g = "Hello\tWorld\tI\tLove\tPython"
+print(g.expandtabs(2))
+
+one = "I Love Python And 3G"
+two = "I Love Python And 3g"
+print(one.istitle())
+print(two.istitle())
+
+three = " "
+four = ""
+print(three.isspace())
+print(four.isspace())
+
+five = "i love python"
+six = "I Love Python"
+print(five.islower())
+print(six.islower())
+
+seven = "Islam_Raad"
+eight = "IslamRaad100"
+nine = "Islam--Raad100"
+
+print(seven.isidentifier())
+print(eight.isidentifier())
+print(nine.isidentifier())
+
+x = "AaaaaBbbbbb"
+y = "AaaaaBbbbbb111"
+print(x.isalpha())
+print(y.isalpha())
+
+u = "AaaaaBbbbbb"
+z = "AaaaaBbbbbb111"
+print(u.isalnum())
+print(z.isalnum())
