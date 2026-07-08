2c6cd05a8b6aba2acfba729edabd4d688bbff2e2
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-08 16:01:01 +0300

    Boolean Methods

--- 1 - Basics/10.py
+++ 1 - Basics/10.py
@@ -37,3 +37,25 @@ print(bool(False))
 print(bool(()))
 print(bool({}))
 print(bool(None))
+
+
+# -----------------------
+# -- Boolean Operators --
+# -----------------------
+# and
+# or
+# not
+# -----------------------
+
+age = 36
+country = "IRAQ"
+rank = 10
+
+print(age > 16 and country == "IRAQ" and rank > 0)  # True
+print(age > 16 and country == "KSA" and rank > 0)  # False
+
+print(age > 40 or country == "KSA" or rank > 20)  # False
+print(age > 40 or country == "IRAQ" or rank > 20)  # True
+
+print(age > 16)  # True
+print(not age > 16)  # Not True = False
