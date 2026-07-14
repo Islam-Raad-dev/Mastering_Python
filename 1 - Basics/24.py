c2fb316e5161d583e0175d55e169e1d309cecee0
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-14 15:59:35 +0300

    Trainings on For Loop

--- 1 - Basics/24.py
+++ 1 - Basics/24.py
@@ -26,3 +26,33 @@ myName = "Osama"
 
 for letter in myName:
     print(f" [ {letter.upper()} ] ")
+
+# -----------------
+# --  Trainings  --
+# -----------------
+
+# Range
+
+myRange = range(1, 101)
+
+for number in myRange:
+    print(number)
+
+# Dictionary
+
+mySkills = {
+    "Html": "90%",
+    "Css": "60%",
+    "PHP": "70%",
+    "JS": "80%",
+    "Python": "90%",
+    "MySQL": "60%",
+}
+
+print(mySkills["JS"])
+print(mySkills.get("Python"))
+
+for skill in mySkills:
+    # print(skill)
+
+    print(f"My Progress in Lang {skill} Is: {mySkills.get(skill)}")
