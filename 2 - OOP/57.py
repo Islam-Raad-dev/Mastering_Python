bcca9d53a1ae205121533495ff9835b6ee75a854
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-19 00:35:09 +0300

    Class Syntax

--- 2 - OOP/57.py
+++ 2 - OOP/57.py
@@ -1,7 +1,7 @@
 # ----------------------------------------------------------
 # -- Object Oriented Programming => Class Syntax and Info --
 # ----------------------------------------------------------
-# [01] Class is The Blueprint Or Construtor Of The Object
+# [01] Class is The Blueprint Or Constructor Of The Object
 # [02] Class Instantiate Means Create Instance of A Class
 # [03] Instance => Object Created From Class And Have Their Methods and Attributes
 # [04] Class Defined With Keyword class
@@ -10,7 +10,7 @@
 # [07] When Creating Object Python Look For The Built In __init__ Method
 # [08] __init__ Method Called Every Time You Create Object From Class
 # [09] __init__ Method Is Initialize The Data For The Object
-# [10] Any Method With Two Underscore in The Start and End Called Dunder or Magic Method
+# [10] Any Method With Two Underscore in The Start and End Called Dander or Magic Method
 # [11] self Refer To The Current Instance Created From The Class And Must Be First Param
 # [12] self Can Be Named Anything
 # [13] In Python You Dont Need To Call new() Keyword To Create Object
@@ -37,8 +37,8 @@ member_three = Member()
 print(member_one.__class__)
 
 my_dictionary = {
-    "name": "Osama",
-    "age": 36,
-    "monthly_salary": 5000,
-    "yearly_salary": "",  # Something
+    "name": "Islam",
+    "age": 20,
+    "monthly_salary": 6000,
+    "yearly_salary": 72000 
 }
