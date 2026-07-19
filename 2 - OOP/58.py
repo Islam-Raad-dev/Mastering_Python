9c392eefe167f6c61dddf2c58e87f1fb68bba91d
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-19 12:47:17 +0300

    Instance Attributes and Methods

---
+++ 2 - OOP/58.py
@@ -0,0 +1,11 @@
+# --------------------------------------------------------------------
+# -- Object Oriented Programming => Instance Attributes and Methods --
+# --------------------------------------------------------------------
+# Self: Point To Instance Created From Class
+# Instance Attributes: Instance Attributes Defined Inside The Constructor
+# -----------------------------------------------------------------------
+# Instance Methods: Take Self Parameter Which Point To Instance Created From Class
+# Instance Methods Can Have More Than One Parameter Like Any Function
+# Instance Methods Can Freely Access Attributes And Methods On The Same Object
+# Instance Methods Can Access The Class Itself
+# -----------------------------------------------------------
