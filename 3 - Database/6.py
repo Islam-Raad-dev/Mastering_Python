126db08db7075f4ecbceff228e78e819c1bd66a9
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-23 06:54:36 +0300

    Add Read User Options

--- 3 - Database/6.py
+++ 3 - Database/6.py
@@ -1,19 +1,32 @@
-Massage = """
-What Do You Want To Do ? 
+class SkillApp:
+
+    Massage = """
+What Do You Want To Do:
+
 1 => Show All Skills
 2 => Add New Skill
 3 => Delete A Skill
 4 => Update Skill Progress
 5 => Quit The App
-Choose Option: 
-"""
-commands_list = [1, 2, 3, 4, 5]
 
-user_input = input(Massage).strip().lower()
+Choose Option: """
 
-while user_input not in commands_list:
-    print(f"\n---Sorry Command {user_input}  is Not Found---\n---Please, Enter Another Choose----")
-    user_input = input(Massage).strip().lower()
+    commands_list = [1, 2, 3, 4, 5]
+
+def Read_User_Options():
+    while True:
+        try:
+            user_input = int(input(SkillApp.Massage))
+
+            if user_input in SkillApp.commands_list:
+
+                return user_input
+            
+            print(f"\n--- Sorry Command {user_input} is Not Found ---")
+
+        except ValueError:
+
+            print("\n--- Invalid Input. Please enter a number ---")
 
 
 def Show_Skills():
@@ -37,20 +50,23 @@ def Quit():
 
 #____________________________________________________________________________
 
+def Performed_User_Options(user_input):
+
+    if user_input in SkillApp.commands_list:
+
+        match user_input:
+            case 1:
+                Show_Skills()
+            case 2:
+                Add_Skill()
+            case 3:
+                Delete_Skill()
+            case 4:
+                Update_Skill()
+            case 5: 
+                Quit()
 
 
-if user_input in commands_list:
-    if user_input == 1:
-        Show_Skills()
 
-    elif user_input == 2:
-        Add_Skill()
-
-    elif user_input == 3:
-        Delete_Skill()
-
-    elif user_input == 4:
-        Update_Skill()
-
-    elif user_input == 5:
-        Quit()
+option = Read_User_Options()
+Performed_User_Options(option)
