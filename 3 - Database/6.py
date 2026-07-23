e006b979c625282b56c375301d9125340f8f584f
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-23 06:23:58 +0300

    Edit : Command Input

--- 3 - Database/6.py
+++ 3 - Database/6.py
@@ -1,18 +1,18 @@
 Massage = """
 What Do You Want To Do ? 
-"s" => Show All Skills
-"a" => Add New Skill
-"d" => Delete A Skill
-"u" => Update Skill Progress
-"q" => Quit The App
+1 => Show All Skills
+2 => Add New Skill
+3 => Delete A Skill
+4 => Update Skill Progress
+5 => Quit The App
 Choose Option: 
 """
-commands_list = ["s", "a", "d", "u", "q"]
+commands_list = [1, 2, 3, 4, 5]
 
 user_input = input(Massage).strip().lower()
 
 while user_input not in commands_list:
-    print(f"Sorry Command {user_input}  is Not Found\n Enter Another Choose:")
+    print(f"\n---Sorry Command {user_input}  is Not Found---\n---Please, Enter Another Choose----")
     user_input = input(Massage).strip().lower()
 
 
@@ -40,17 +40,17 @@ def Quit():
 
 
 if user_input in commands_list:
-    if user_input == "s":
+    if user_input == 1:
         Show_Skills()
 
-    elif user_input == "a":
+    elif user_input == 2:
         Add_Skill()
 
-    elif user_input == "d":
+    elif user_input == 3:
         Delete_Skill()
 
-    elif user_input == "u":
+    elif user_input == 4:
         Update_Skill()
 
-    elif user_input == "q":
+    elif user_input == 5:
         Quit()
