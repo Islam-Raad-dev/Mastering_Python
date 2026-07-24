caca9a5048895a1cb97e37c31059b61a9f776095
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-24 23:11:06 +0300

    Add skills page and update skills data structure

--- 5 - Flask/Main.py
+++ 5 - Flask/Main.py
@@ -6,6 +6,8 @@ from flask import Flask, render_template
 
 skills_app = Flask(__name__)
 
+my_skills = [("Html", 80), ("CSS", 75), ("Python", 95)]
+
 
 @skills_app.route("/")
 def homepage():
@@ -25,6 +27,16 @@ def about():
     return render_template("about.html",
                             pagetitle = "About")
 
+
+@skills_app.route("/skills")
+def skills():
+    return render_template(
+        "skills.html",
+        title="My Skills",
+        page_head="My Skills",
+        description="This Is My Skills Page",
+        skills=my_skills,
+    )
 
 if __name__ == "__main__":
     skills_app.run(debug=True, port=9000)
