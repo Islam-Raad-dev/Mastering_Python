d8601db1baec4038fde69972f5bd662019524201
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-25 13:42:01 +0300

    Compare Data Location And Type

--- 6 - Numpy/3.py
+++ 6 - Numpy/3.py
@@ -2,23 +2,47 @@
 # -- Numpy => Compare Data Location And Type --
 # ---------------------------------------------
 
-import numpy as np  # noqa: F401
+import numpy as np
+
+my_list = [1, 2, 3, 4, 5]
+my_array = np.array([1, 2, 3, 4, 5])
+
+print(my_list[0])
+print(my_list[1])
+
+
+print(my_array[0])
+print(my_array[1])
 
 print("#" * 50)
 
+print(id(my_list[0]))
+print(id(my_list[1]))
+
+print(id(my_array[0]))
+print(id(my_array[1]))
 
 
 print("#" * 50)
 
+my_list_of_data = [1,"A", 2, "B", True, 10.20]
+my_array_of_data = np.array([2, False, "A", "B", True, 10.20])
+print("#" * 50)
+
+print(my_list_of_data)
+print(my_array_of_data)
+
+print("#" * 50)
+
+print(my_list_of_data[0])
+print(my_list_of_data[1])
+
+
+print(my_array_of_data[0])
+print(my_array_of_data[1])
 
 
 print("#" * 50)
 
-
-
-print("#" * 50)
-
-
-print("#" * 50)
-
-
+print(type(my_list_of_data[0]))
+print(type(my_array_of_data[0]))
