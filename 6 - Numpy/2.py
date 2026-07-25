9f0f97ed2de951328e9b9f658c816f5fccda5cc1
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-25 12:22:50 +0300

    Create Arrays

---
+++ 6 - Numpy/2.py
@@ -0,0 +1,39 @@
+# ----------------------------
+# -- Numpy => Create Arrays --
+# ----------------------------
+
+
+
+# print(dir(np))
+
+
+
+print("#" * 50)
+
+# Type
+
+
+
+print("#" * 50)
+
+# Accessing Elements
+
+
+
+print("#" * 50)
+
+
+
+
+
+print("#" * 50)
+
+# Number Of Dimensions
+
+
+
+print("#" * 50)
+
+# Custom Dimensions
+
+
