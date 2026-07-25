b39432def240eac5dc5037a63989f0e618e5d44d
Islam.Raad.dev <islamraad.dev@gmail.com>
2026-07-25 12:47:18 +0300

    Edit : 1

--- 6 - Numpy/2.py
+++ 6 - Numpy/2.py
@@ -2,28 +2,38 @@
 # -- Numpy => Create Arrays --
 # ----------------------------
 
-
+import numpy as np
 
 # print(dir(np))
 
+my_list = [1, 2, 3, 4, 5]
+my_array = np.array(my_list)
 
+print(my_list)
+print(my_array)
 
 print("#" * 50)
 
 # Type
 
-
+print(type(my_list))
+print(type(my_array))
 
 print("#" * 50)
 
 # Accessing Elements
 
-
+print(my_list[0])
+print(my_array[0])
 
 print("#" * 50)
 
+a = np.array(10)
+b = np.array([10, 20])
+c = np.array([[1, 2], [4, 5]])
+d = np.array([[[3, 2], [3, 4]], [[5, 6], [7, 8]]])
 
-
+print(d[1, 1, -1])
 
 
 print("#" * 50)
