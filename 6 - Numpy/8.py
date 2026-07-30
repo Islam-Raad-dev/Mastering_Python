f0a28fc2005e85b8ce291eb1fde41e1f218c9842
Islam Raad <islamraad.dev@gmail.com>
2026-07-30 23:22:42 +0300

    Change first element of my_array4 to 0

--- 6 - Numpy/8.py
+++ 6 - Numpy/8.py
@@ -22,7 +22,7 @@ print(my_array3.shape)
 
 print("#" * 50)
 
-my_array4 = np.array([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+my_array4 = np.array([0 ,1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
 print(my_array4.ndim)
 print(my_array4.shape)
 
